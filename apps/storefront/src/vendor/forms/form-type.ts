555020964e1f60929ed9111889b44538ffb45d40
Yago Russo <yagrrusso@gmail.com>
2026-09-02 10:19:33 -0300

    feat: redefine CommonFormProps and CommonFormValuesProps for enhanced form state management

--- apps/storefront/src/vendor/forms/form-type.ts
+++ apps/storefront/src/vendor/forms/form-type.ts
@@ -1,5 +1,21 @@
-export type CommonFormProps<TValues> = {
-  defaultValues?: Partial<TValues>
+export type FormState = "CREATING" | "READING" | "UPDATING" | "DELETING"
+
+export interface CommonFormValuesProps<
+  Default = Record<string, any>,
+  Custom = Record<string, any>,
+> {
+  customValues?: Partial<Custom>
+  defaultValues?: Partial<Default>
+  isDisabled?: boolean
   isLoading?: boolean
-  onSubmit: (values: TValues) => void | Promise<void>
+  state?: FormState
+}
+
+export interface CommonFormProps<
+  Default = Record<string, any>,
+  Custom = Record<string, any>,
+> extends CommonFormValuesProps<Default, Custom> {
+  onCancel?: () => void
+  onSubmit?: (data: Default & Partial<Custom>) => void
+  onError?: (error: string) => void
 }
